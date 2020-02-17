d35f9818bf2ca18592e983c371595ef1a84244e2
Kirk Easterson <kirk.easterson@gmail.com>
2020-02-17 23:32:24 +0100

    modify FeedbackQuestionAttributesText.java fixes #32

--- src/test/java/teammates/test/cases/datatransfer/FeedbackQuestionAttributesTest.java
+++ src/test/java/teammates/test/cases/datatransfer/FeedbackQuestionAttributesTest.java
@@ -856,4 +856,40 @@ public class FeedbackQuestionAttributesTest extends BaseAttributesTest {
         FeedbackQuestionAttributes fqa2 = fqa1.getCopy();
         assertEquals(fqa1, fqa2);
     }
+
+    @Test
+    public void testEqualsWithSameObjectShouldPass() {
+        FeedbackQuestionAttributes fqa1 =
+                FeedbackQuestionAttributes.builder()
+                        .withCourseId("courseId")
+                        .withFeedbackSessionName("session")
+                        .withGiverType(FeedbackParticipantType.INSTRUCTORS)
+                        .withRecipientType(FeedbackParticipantType.SELF)
+                        .withNumberOfEntitiesToGiveFeedbackTo(3)
+                        .withQuestionNumber(1)
+                        .withQuestionDetails(new FeedbackTextQuestionDetails("question text"))
+                        .withShowGiverNameTo(new ArrayList<>())
+                        .withShowRecipientNameTo(new ArrayList<>())
+                        .withShowResponsesTo(new ArrayList<>())
+                        .build();
+        assertEquals(fqa1, fqa1);
+    }
+
+    @Test
+    public void testEqualsWithDifferentClassShouldFail() {
+        FeedbackQuestionAttributes fqa1 =
+                FeedbackQuestionAttributes.builder()
+                        .withCourseId("courseId")
+                        .withFeedbackSessionName("session")
+                        .withGiverType(FeedbackParticipantType.INSTRUCTORS)
+                        .withRecipientType(FeedbackParticipantType.SELF)
+                        .withNumberOfEntitiesToGiveFeedbackTo(3)
+                        .withQuestionNumber(1)
+                        .withQuestionDetails(new FeedbackTextQuestionDetails("question text"))
+                        .withShowGiverNameTo(new ArrayList<>())
+                        .withShowRecipientNameTo(new ArrayList<>())
+                        .withShowResponsesTo(new ArrayList<>())
+                        .build();
+        assertEquals(fqa1, new Object());
+    }
 }
